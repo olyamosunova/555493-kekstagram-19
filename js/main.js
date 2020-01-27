14c57c81913bf8b58a0f16d8381343101f12bdcf
Tarasiy <dirigablenorge@gmail.com>
2020-01-27 11:07:01 +0400

    fix mistakes

--- js/main.js
+++ js/main.js
@@ -56,97 +56,98 @@ var NAME_TEMPLATES = [
   'Вуглусрах'
 ];
 
-var getRandomElementFromArray = function (array) {
-  var _min = Math.floor(Math.random() * 2);
-  var _max = array.length;
-  var _randomNumber = Math.floor(Math.random() * (_max - _min / 2));
+var AVATAR_NUMBERS = [1, 2, 3, 4, 5, 6];
+var MIN_COMMENTS = 3;
+var MAX_COMMENTS = 15;
+var MIN_LIKES = 10;
+var MAX_LIKES = 200;
 
-  _randomNumber = _randomNumber < 0 ? 0 : _randomNumber;
+var getRandomElementFromArray = function (array) {
+  var min = Math.floor(Math.random() * 2);
+  var max = array.length;
+  var randomNumber = Math.floor(Math.random() * (max - min / 2));
+
+  randomNumber = randomNumber < 0 ? 0 : randomNumber;
 
   array = Math.floor(Math.random() * 2) === 0 ? array.reverse() : array;
 
-  return array[_randomNumber];
+  return array[randomNumber];
 };
 
-var getAuthorName = function (names) {
-  return getRandomElementFromArray(names);
+var getAvatarURL = function (avatarNumbers) {
+  return 'img\\avatar-' + getRandomElementFromArray(avatarNumbers) + '.svg';
 };
 
-var getAvatarURL = function () {
-  return 'img\\avatar-' + getRandomElementFromArray([1, 2, 3, 4, 5, 6]) + '.svg';
-};
-
-var createMessage = function (messageTemplates, names) {
-  var _message = Math.floor(Math.random() * 2) + 1 === 1 ? getRandomElementFromArray(messageTemplates) : getRandomElementFromArray(messageTemplates) + getRandomElementFromArray(messageTemplates);
+var createMessage = function (messageTemplates, names, avatarNumbers) {
+  var message = Math.floor(Math.random() * 2) + 1 === 1 ? getRandomElementFromArray(messageTemplates) : getRandomElementFromArray(messageTemplates) + getRandomElementFromArray(messageTemplates);
 
   return {
-    'avatar': getAvatarURL(),
-    'message': _message,
-    'name': getAuthorName(names)
+    avatar: getAvatarURL(avatarNumbers),
+    message: message,
+    name: getRandomElementFromArray(names)
   };
 };
 
-var createMessages = function (quantity, messageTemplates, names) {
-  var _messages = [];
+var createMessages = function (quantity, messageTemplates, names, avatarNumbers) {
+  var messages = [];
 
   for (var i = 0; i < quantity; i++) {
-    _messages.push(createMessage(messageTemplates, names));
+    messages.push(createMessage(messageTemplates, names, avatarNumbers));
   }
 
-  return _messages;
+  return messages;
 };
 
-var MESSAGES = createMessages(QUANTITY_MESSAGES, MESSAGE_TEMPLATES, NAME_TEMPLATES);
+var mokiMessages = createMessages(QUANTITY_MESSAGES, MESSAGE_TEMPLATES, NAME_TEMPLATES, AVATAR_NUMBERS);
 
 var getPhotoURL = function (number) {
   return 'photos/' + number + '.jpg';
 };
 
-var getQuantityLikes = function (max, min) {
-  return Math.floor(Math.random() * (max - min)) + min;
+var getQuantityLikes = function (minLikes, maxLikes) {
+  return Math.floor(Math.random() * (maxLikes - minLikes)) + minLikes;
 };
 
-var getComments = function (messages) {
-  var _max = 15;
-  var _min = 3;
-  var _randomQuantityComments = Math.floor(Math.random() * (_max - _min)) + _min;
-  var _comments = [];
+var getComments = function (messages, minMessages, maxMessages) {
+  var randomQuantityComments = Math.floor(Math.random() * (maxMessages - minMessages)) + minMessages;
+  var comments = [];
 
-  for (var i = 0; i < _randomQuantityComments; i++) {
-    _comments.push(getRandomElementFromArray(messages));
+  for (var i = 0; i < randomQuantityComments; i++) {
+    comments.push(getRandomElementFromArray(messages));
   }
 
-  return _comments;
+  return comments;
 };
 
-var createMOKI = function (quantity) {
-  var _records = [];
+var renderMOKI = function (quantity, messages) {
+  var records = [];
 
   for (var i = 0; i < quantity; i++) {
-    _records.push({
-      'url': getPhotoURL(i + 1),
-      'description': ' ',
-      'likes': getQuantityLikes(200, 15),
-      'comments': getComments(MESSAGES),
+    records.push({
+      url: getPhotoURL(i + 1),
+      description: ' ',
+      likes: getQuantityLikes(MIN_LIKES, MAX_LIKES),
+      comments: getComments(messages, MIN_COMMENTS, MAX_COMMENTS),
     });
   }
 
-  return _records;
+  return records;
 };
-
-var MOKI = createMOKI(QUANTITY_RECORDS);
 
 var picturesFragment = document.createDocumentFragment();
 var templatePicture = document.querySelector('#picture');
 
+var renderPictures = function (mokiData, template) {
+  for (var i = 0; i < mokiData.length; i++) {
+    var pictureElement = template.cloneNode(true).content;
+    pictureElement.querySelector('.picture__img').src = mokiData[i].url;
+    pictureElement.querySelector('.picture__likes').textContent = mokiData[i].likes;
+    pictureElement.querySelector('.picture__comments').textContent = mokiData[i].comments.length;
 
-for (var i = 0; i < MOKI.length; i++) {
-  var pictureElement = templatePicture.cloneNode(true).content;
-  pictureElement.querySelector('.picture__img').src = MOKI[i].url;
-  pictureElement.querySelector('.picture__likes').textContent = MOKI[i].likes;
-  pictureElement.querySelector('.picture__comments').textContent = MOKI[i].comments.length;
+    picturesFragment.appendChild(pictureElement);
+  }
+};
 
-  picturesFragment.appendChild(pictureElement);
-}
+renderPictures(renderMOKI(QUANTITY_RECORDS, mokiMessages), templatePicture);
 
 document.querySelector('.pictures').appendChild(picturesFragment);
